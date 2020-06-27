84b381f240dacdf443959786ef26410c30a0b304
Kay <Kay.com>
2020-06-27 12:39:02 +0800

    06271238

--- jackson_parent/jackson_service_provider/src/main/java/com/it/jackson0419/vo/User.java
+++ jackson_parent/jackson_service_provider/src/main/java/com/it/jackson0419/vo/User.java
@@ -18,6 +18,7 @@ public class User {
     private String name;
     private String cnName;
     private String enName;
+    private String password;
 
     //    @Column(name = "age")
     private int age;
